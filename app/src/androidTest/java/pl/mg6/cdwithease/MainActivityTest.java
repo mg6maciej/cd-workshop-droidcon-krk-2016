156b82c3795a050d845672bd1c872ed80d7a440e
Maciej Górski <maciek.gorski@gmail.com>
2016-12-10 12:16:29 +0100

    Test MainActivity.

---
+++ app/src/androidTest/java/pl/mg6/cdwithease/MainActivityTest.java
@@ -0,0 +1,23 @@
+package pl.mg6.cdwithease;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldDisplayHelloWorld() {
+        onView(withId(R.id.text_view))
+                .check(matches(withText("Hello World!")));
+    }
+}
